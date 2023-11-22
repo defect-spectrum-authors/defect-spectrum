2c27e2c2a76d2766966edcc5a062fa4801c4a73c
defect-spectrum-authors <150683350+defect-spectrum-authors@users.noreply.github.com>
2023-11-22 13:35:27 +0800

    Updated

--- inference.sh
+++ inference.sh
@@ -5,9 +5,9 @@ python -m torch.distributed.launch \
 --master_port=29511 \
 inference_for_perception.py \
 --step_inference 400 \
---sample_dir '/home/zfchen/working/diff_aug/generation_for_perception_test/screw_exp4' \
---large_recep '/home/zfchen/working/diff_aug/VISION_dataset/screw_large_recep/checkpoint/diffusion_150000.pt' \
---small_recep '' \
+--sample_dir 'PATH_TO_YOUR_WORKING_DIR' \
+--large_recep 'YOUR_LARGE_RECPETIVE_FIELD_CKPT' \
+--small_recep 'YOUR_LARGE_RECPETIVE_FIELD_CKPT' \
 --num_defect 3 \
---large_recep_config '/home/zfchen/working/diff_aug/config/large_recep_vision_screw.yml' \
---small_recep_config '/home/zfchen/working/diff_aug/config/small_recep.yml' \
+--large_recep_config 'YOUR_LARGE_RECEP_CONFIG' \
+--small_recep_config 'YOUR_SMALL_RECEP_CONFIG' \
